a249999c5c85025e91fdede732ff3c742aafe83f
Jeroen Vannevel <jer_vannevel@outlook.com>
2019-07-01 10:59:19 +0100

    Added generation of webvtt file

--- SpriteGenerator/Program.cs
+++ SpriteGenerator/Program.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 
 namespace SpriteGenerator
@@ -21,8 +22,8 @@ namespace SpriteGenerator
 
             Console.WriteLine("Generating thumbnails");
             Directory.CreateDirectory("output");
-            // One thumbnail every 2 seconds
-            var generateThumbnailArgs = "-ss 00:00:00 -i 5aa149aa2ab18726bc998c6a.mp4 -vf fps=1/2 output/preview-%5d.png";
+            // One thumbnail every 5 seconds
+            var generateThumbnailArgs = "-ss 00:00:00 -i 5aa149aa2ab18726bc998c6a.mp4 -vf fps=1/5 output/preview-%5d.png";
             var ffmpeg = Process.Start("ffmpeg.exe", generateThumbnailArgs);
             ffmpeg.WaitForExit();
 
@@ -40,23 +41,33 @@ namespace SpriteGenerator
             // ffmpeg might still be writing the output files
             Thread.Sleep(2000);
 
-            // One bitmap contains 12 images or 24 seconds of video
+            // One bitmap contains 12 images or 60 seconds of video
             // Images are laid out in 3 columns and 4 rows
             Console.WriteLine("Generating sprites");
             Directory.CreateDirectory("output/sprites");
             var batches = Directory.EnumerateFiles("output/scaled").InSetsOf(12);
+            var thumbnailTrack = new StringBuilder();
+            thumbnailTrack.AppendLine("WEBVTT");
+            thumbnailTrack.AppendLine();
+
             foreach (var batch in batches)
             {
-                GenerateBitmap(batch);
+                GenerateBitmap(batch, thumbnailTrack);
             }
+
+            GenerateThumbnailTrack(thumbnailTrack);
 
             Console.Read();
         }
 
-        private static void GenerateBitmap(List<string> files)
+        private static void GenerateThumbnailTrack(StringBuilder thumbnailTrack) => File.WriteAllText("output/previews.vtt", thumbnailTrack.ToString());
+
+        private static void GenerateBitmap(List<string> files, StringBuilder thumbnailTrack)
         {
+            var timePerImage = 5;
             var firstImage = Image.FromFile(files[0]);
             var lastTimestamp = GetTimestamp(files[files.Count - 1]);
+            var firstTimestamp = GetTimestamp(files[0]) - timePerImage;
             var spriteName = $"{lastTimestamp}.png";
 
             using (var currentBitmap = new Bitmap(firstImage.Width * 3, firstImage.Height * 4))
@@ -64,11 +75,20 @@ namespace SpriteGenerator
             {
                 int currentX = 0, currentY = 0;
                 int imagesInBitmap = 0;
+                var currentStartTimestamp = TimeSpan.FromSeconds(firstTimestamp);
+                var currentEndTimestamp = currentStartTimestamp;
                 foreach (var file in files)
                 {
                     var image = Image.FromFile(file);
+                    currentEndTimestamp = currentStartTimestamp.Add(TimeSpan.FromSeconds(timePerImage));
 
                     canvas.DrawImage(image, currentX, currentY);
+
+                    var currentStartTimeString = currentStartTimestamp.ToString(@"hh\:mm\:ss\.fff");
+                    var currentEndTimeString = currentEndTimestamp.ToString(@"hh\:mm\:ss\.fff");
+                    thumbnailTrack.AppendLine($"{currentStartTimeString} --> {currentEndTimeString}");
+                    thumbnailTrack.AppendLine($"{spriteName}#xywh={currentX},{currentY},{firstImage.Width},{firstImage.Height}");
+                    thumbnailTrack.AppendLine();
 
                     imagesInBitmap++;
                     if (imagesInBitmap % 3 == 0)
@@ -80,6 +100,8 @@ namespace SpriteGenerator
                     {
                         currentX += image.Width;
                     }
+
+                    currentStartTimestamp = currentEndTimestamp;
                 }
 
                 Console.WriteLine($"Writing sprite: {spriteName}");
@@ -91,8 +113,8 @@ namespace SpriteGenerator
         {
             var nakedFilename = Path.GetFileNameWithoutExtension(file);
             var timestamp = int.Parse(nakedFilename.Substring(nakedFilename.LastIndexOf("_") + 1));
-            // 1 frame every 2 seconds
-            return timestamp * 2;
+            // 1 frame every 5 seconds
+            return timestamp * 5;
         }
     }
 
